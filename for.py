61aaadc78f89d846ca39d7c44b7f2721fb72092b
Saber MalekzadeH <saber.malekzade@gmail.com>
2021-01-31 16:35:07 +0400

    Update for.py

--- for.py
+++ for.py
@@ -1,6 +1,33 @@
+import requests
+import json
 import time
-i = 0
+
+
+class bot():
+
+    def __init__(self):
+        self.token = '1664699228:AAHQvvzDD8hl0TweKRKAbPSB0mmbSOn9xbc'
+        self.base = "https://api.telegram.org/bot{}/".format(self.token)
+
+    def get_updates(self, offset=None):
+        url = self.base + "getUpdates?timeout=100"
+        if offset:
+            url = url + "&offset={}".format(offset + 1)
+        r = requests.get(url)
+        return json.loads(r.content)
+
+    def send_message(self, msg, chat_id):
+        url = self.base + "sendMessage?chat_id={}&text={}".format(chat_id, msg)
+        if msg is not None:
+            requests.get(url)
+
+b = bot()
+
+
+def fw():
+  for i in range(1):
+    b.send_message('Hi. Now is     '+time.asctime( time.localtime(time.time())),104710146)
+
 while True:
-  print(i)
-  i += 1
-  time.sleep(1)
+    fw()
+    time.sleep(5)
